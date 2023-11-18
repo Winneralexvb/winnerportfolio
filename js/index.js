c709b3481fe7943d22a1c8984578ddeb1982dd93
Winneralexvb <149628161+Winneralexvb@users.noreply.github.com>
2023-11-18 03:37:49 -0300

    Big Update Mobile

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var navbar = document.querySelector("header nav");
+    var navbar = document.querySelector("header nav.menu-nav");
+    var toposite = document.querySelector("header .toposite");    
     var isScrolled = false;
 
     window.addEventListener("scroll", function () {
@@ -8,11 +9,18 @@ document.addEventListener("DOMContentLoaded", function () {
         if (scrollPosition > 70 && !isScrolled) {
             navbar.classList.add("scrolled");
             navbar.classList.add("fixed");
+            toposite.classList.add("fixed")
             isScrolled = true;
         } else if (scrollPosition <= 70 && isScrolled) {
             navbar.classList.remove("scrolled");
             navbar.classList.remove("fixed");
+            toposite.classList.remove("fixed");
             isScrolled = false;
         }
     });
 });
+
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
---
+++ js/menumobile.js
@@ -0,0 +1,15 @@
+let btnMenu = document.getElementById('abrir')
+let menu = document.getElementById('navmobile')
+let overlay = document.getElementById('overlay-mobile')
+
+btnMenu.addEventListener('click',()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu')
+})
