86d17c8f63abdbadf210c348cb67371147f5d021
Winneralexvb <149628161+Winneralexvb@users.noreply.github.com>
2023-11-29 09:51:06 -0300

    update contact menu

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var navbar = document.querySelector("header nav.menu-nav");
     var toposite = document.querySelector("header .toposite");
     var btnabrir = document.querySelector(".btn-abrir i");
+    var btnphone = document.querySelector(".btn-phone");
     var isScrolled = false;
 
     window.addEventListener("scroll", function () {
@@ -12,12 +13,14 @@ document.addEventListener("DOMContentLoaded", function () {
             navbar.classList.add("fixed");
             toposite.classList.add("fixed");
             btnabrir.classList.add("fixed");
+            btnphone.classList.add("fixed");
             isScrolled = true;
         } else if (scrollPosition <= 70 && isScrolled) {
             navbar.classList.remove("scrolled");
             navbar.classList.remove("fixed");
             toposite.classList.remove("fixed");
             btnabrir.classList.remove("fixed");
+            btnphone.classList.remove("fixed");
             isScrolled = false;
         }
     });
